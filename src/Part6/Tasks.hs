834691ef2d1a93e1580d0be826329e29d2ee33ee
vityaman <vityaman.dev@yandex.ru>
2025-12-28 14:45:39 +0300

    Solve multiplyMatrix
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- src/Part6/Tasks.hs
+++ src/Part6/Tasks.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Part6.Tasks where
 
-import Data.Map
+import Data.Either (fromRight)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe, listToMaybe)
 import Util (notImplementedYet)
 
 -- Разреженное представление матрицы. Все элементы, которых нет в
@@ -14,33 +20,169 @@ data SparseMatrix a = SparseMatrix
   }
   deriving (Show, Eq)
 
+type MatrixResult a = Either String a
+
 -- Определите класс типов "Матрица" с необходимыми (как вам кажется)
 -- операциями, которые нужны, чтобы реализовать функции, представленные ниже
-class Matrix mx
+class Matrix mx where
+  type MatrixElement mx :: *
+
+  matrixZero :: Int -> Int -> MatrixResult mx
+  matrixHeight :: mx -> Int
+  matrixWidth :: mx -> Int
+  matrixGet' :: Int -> Int -> mx -> MatrixElement mx
+  matrixPut' :: Int -> Int -> MatrixElement mx -> mx -> mx
+
+matrixGet :: (Matrix mx) => Int -> Int -> mx -> MatrixResult (MatrixElement mx)
+matrixGet i j m = do
+  _ <- matrixCheckRange i j m
+  return $ matrixGet' i j m
+
+matrixPut :: (Matrix mx) => Int -> Int -> MatrixElement mx -> mx -> MatrixResult mx
+matrixPut i j e m = do
+  _ <- matrixCheckRange i j m
+  return $ matrixPut' i j e m
+
+matrixCheckRange :: (Matrix mx) => Int -> Int -> mx -> MatrixResult ()
+matrixCheckRange i j m
+  | i < 0 || j < 0 = Left $ "Negative index " ++ show (i, j)
+  | h < i || w < j = Left $ "Out of range index " ++ show (i, j)
+  | otherwise = Right ()
+  where
+    h = matrixHeight m
+    w = matrixWidth m
+
+matrixSize :: (Matrix mx) => mx -> (Int, Int)
+matrixSize m = (matrixHeight m, matrixWidth m)
+
+matrixNew :: (Matrix mx) => Int -> Int -> (Int -> Int -> MatrixElement mx) -> MatrixResult mx
+matrixNew h w e = do
+  z <- matrixZero h w
+  let is = [(i, j) | i <- [0 .. h - 1], j <- [0 .. w - 1]]
+      put (i, j) = matrixPut' i j (e i j)
+  return $ foldl (flip put) z is
+
+matrixProduct :: (Matrix mx, Num (MatrixElement mx)) => mx -> mx -> MatrixResult mx
+matrixProduct lhs rhs
+  | n /= n1 =
+      Left $
+        "Product incompatible "
+          ++ (show (m, n) ++ " and " ++ show (n1, p))
+  | otherwise = matrixNew m p c
+  where
+    ((m, n), (n1, p)) = (matrixSize lhs, matrixSize rhs)
+    a i j = matrixGet' i j lhs
+    b i j = matrixGet' i j rhs
+    c i j = sum $ fmap (\k -> a i k * b k j) [0 .. n - 1]
 
 -- Определите экземпляры данного класса для:
 --  * числа (считается матрицей 1x1)
 --  * списка списков чисел
 --  * типа SparseMatrix, представленного выше
-instance Matrix Int
+instance Matrix Int where
+  type MatrixElement Int = Int
 
-instance Matrix [[Int]]
+  matrixZero :: Int -> Int -> MatrixResult Int
+  matrixZero 1 1 = Right 0
+  matrixZero h w =
+    Left $ "Matrix Int can be only (1, 1), " ++ ("not " ++ show (h, w))
 
-instance Matrix (SparseMatrix Int)
+  matrixHeight :: Int -> Int
+  matrixHeight _ = 1
+
+  matrixWidth :: Int -> Int
+  matrixWidth _ = 1
+
+  matrixGet' :: Int -> Int -> Int -> Int
+  matrixGet' 0 0 m = m
+
+  matrixPut' :: Int -> Int -> Int -> Int -> Int
+  matrixPut' 0 0 e _ = e
+
+instance (Num a) => Matrix [[a]] where
+  type MatrixElement [[a]] = a
+
+  matrixZero :: Int -> Int -> MatrixResult [[a]]
+  matrixZero h w
+    | h <= 0 || w <= 0 = Left $ "Non-positive size " ++ show (h, w)
+    | otherwise = Right $ [[0 | j <- [1 .. w]] | i <- [1 .. h]]
+
+  matrixHeight :: [[a]] -> Int
+  matrixHeight = length
+
+  matrixWidth :: [[a]] -> Int
+  matrixWidth m = maximum $ fmap length m
+
+  matrixGet' :: Int -> Int -> [[a]] -> a
+  matrixGet' 0 j (row : _) = fromMaybe 0 $ listToMaybe (drop j row)
+  matrixGet' i j (_ : rows) = matrixGet' (i - 1) j rows
+
+  matrixPut' :: Int -> Int -> a -> [[a]] -> [[a]]
+  matrixPut' 0 j e (cols : rows) = replaceNth j e cols : rows
+    where
+      replaceNth i e [] = replicate i 0 ++ [e]
+      replaceNth 0 e (x : xs) = e : xs
+      replaceNth i e (x : xs) = x : replaceNth (i - 1) e xs
+  matrixPut' i j e (cols : rows) = cols : matrixPut' (i - 1) j e rows
+
+instance (Num a, Eq a) => Matrix (SparseMatrix a) where
+  type MatrixElement (SparseMatrix a) = a
+
+  matrixZero :: Int -> Int -> MatrixResult (SparseMatrix a)
+  matrixZero h w
+    | h <= 0 || w <= 0 = Left $ "Non-positive size " ++ show (h, w)
+    | otherwise = Right $ SparseMatrix w h Map.empty
+
+  matrixHeight :: SparseMatrix a -> Int
+  matrixHeight = sparseMatrixHeight
+
+  matrixWidth :: SparseMatrix a -> Int
+  matrixWidth = sparseMatrixWidth
+
+  matrixGet' :: Int -> Int -> SparseMatrix a -> MatrixElement (SparseMatrix a)
+  matrixGet' i j = Map.findWithDefault 0 (i, j) . sparseMatrixElements
+
+  matrixPut' :: Int -> Int -> MatrixElement (SparseMatrix a) -> SparseMatrix a -> SparseMatrix a
+  matrixPut' i j e m = SparseMatrix w h $ put e es
+    where
+      h = matrixHeight m
+      w = matrixWidth m
+      es = sparseMatrixElements m
+      put 0 = Map.delete (i, j)
+      put e = Map.insert (i, j) e
 
 -- Реализуйте следующие функции
 -- Единичная матрица
-eye :: (Matrix m) => Int -> m
-eye w = notImplementedYet
+eye :: (Matrix m, Num (MatrixElement m)) => Int -> m
+eye w = case matrixNew w w e of
+  Right m -> m
+  Left m -> error m
+  where
+    e i j
+      | i == j = 1
+      | otherwise = 0
 
 -- Матрица, заполненная нулями
 zero :: (Matrix m) => Int -> Int -> m
-zero w h = notImplementedYet
+zero w h = case matrixZero h w of
+  Right m -> m
+  Left m -> error m
 
 -- Перемножение матриц
-multiplyMatrix :: (Matrix m) => m -> m -> m
-multiplyMatrix = notImplementedYet
+multiplyMatrix :: (Matrix m, Num (MatrixElement m)) => m -> m -> m
+multiplyMatrix a b = case matrixProduct a b of
+  Right m -> m
+  Left m -> error m
 
 -- Определитель матрицы
 determinant :: (Matrix m) => m -> Int
 determinant = notImplementedYet
+
+fromLoL :: (Num a, Eq a) => [[a]] -> SparseMatrix a
+fromLoL lol = case matrixNew h w e of
+  Right m -> m
+  Left m -> error m
+  where
+    h = matrixHeight lol
+    w = matrixWidth lol
+    e i j = matrixGet' i j lol
--- src/Part6/Tests.hs
+++ src/Part6/Tests.hs
@@ -21,3 +21,41 @@ unit_zero = do
   zero 5 5 @?= SparseMatrix 5 5 (Data.Map.fromList ([] :: [((Int, Int), Int)]))
   where
     zz :: Int; zz = 0
+
+-- Thanks, dmfrpro!
+unit_multiplyMatrix = do
+  let fromList2D = fromLoL
+      getElem m i j = matrixGet' i j m
+
+  multiplyMatrix (2 :: Int) (3 :: Int) @?= 6
+
+  let a = [[1, 2], [3, 4]] :: [[Int]]
+  let b = [[5, 6], [7, 8]] :: [[Int]]
+  multiplyMatrix a b @?= [[19, 22], [43, 50]]
+
+  let c = [[1, 2, 3], [4, 5, 6]] :: [[Int]]
+  let d = [[7, 8], [9, 10], [11, 12]] :: [[Int]]
+  multiplyMatrix c d @?= [[58, 64], [139, 154]]
+
+  multiplyMatrix a (eye 2 :: [[Int]]) @?= a
+  multiplyMatrix (eye 2 :: [[Int]]) a @?= a
+
+  multiplyMatrix a (zero 2 2 :: [[Int]]) @?= zero 2 2
+  multiplyMatrix (zero 2 2 :: [[Int]]) a @?= zero 2 2
+
+  let sparseA = fromList2D a :: SparseMatrix Int
+  let sparseB = fromList2D b :: SparseMatrix Int
+  let result = multiplyMatrix sparseA sparseB
+  getElem result 0 0 @?= 19
+  getElem result 0 1 @?= 22
+  getElem result 1 0 @?= 43
+  getElem result 1 1 @?= 50
+
+  multiplyMatrix sparseA (eye 2 :: SparseMatrix Int) @?= sparseA
+
+  let sparseMat = fromList2D [[1, 0, 2], [0, 3, 0], [4, 0, 5]] :: SparseMatrix Int
+  let sparseMat2 = fromList2D [[1, 0, 0], [0, 2, 0], [0, 0, 3]] :: SparseMatrix Int
+  let sparseResult = multiplyMatrix sparseMat sparseMat2
+  getElem sparseResult 0 0 @?= 1
+  getElem sparseResult 1 1 @?= 6
+  getElem sparseResult 2 2 @?= 15
